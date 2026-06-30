f4057a751eede04e69265ffe53d9c556b9118fbb
kamalikamano123 <kamalikamano0615@gmail.com>
2026-06-30 21:43:15 +0530

    Update backend and frontend

--- backend/main.py
+++ backend/main.py
@@ -22,7 +22,7 @@ app = FastAPI()
 # ✅ CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://qa-chatbot-seven.vercel.app/"],
+    allow_origins=["http://localhost:5173","https://qa-chatbot-seven.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
